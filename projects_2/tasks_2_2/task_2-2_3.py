6c5fce54dc94ddc3550ae45cb12704cea09f787d
gordeevayaroslava09-sudo <gordeevayaroslava09@gmail.com>
2026-02-19 14:10:17 +0300

    Add files via upload

---
+++ projects_2/tasks_2_2/task_2-2_3.py
@@ -0,0 +1,5 @@
+device_name = 'Microscope'
+inventory_number = 3401
+device_condition = "Исправен"
+device_qualitity = 3
+print('Название прибора\t', device_name, '\nИнвентарный номер\t', inventory_number, '\nСостояние прибора\t', device_condition, '\nКоличество\t', device_qualitity)
